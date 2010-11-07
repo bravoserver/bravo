7eb7786050cb396a1f8752a7e9d2ea9bf8409eae
Corbin Simpson <MostAwesomeDude@gmail.com>
2010-11-07 04:30:22 -0800

    ibeta: Add terrain generation interface.

--- beta/ibeta.py
+++ beta/ibeta.py
@@ -2,14 +2,25 @@ from zope.interface import Interface, Attribute
 
 class IAuthenticator(Interface):
 
-    def handshake(protocol, container):
+    def handshake(self, protocol, container):
         """
         Handle a handshake.
         """
 
-    def login(protocol, container):
+    def login(self, protocol, container):
         """
         Handle a login.
+        """
+
+    name = Attribute("""
+        The name of the plugin.
+        """)
+
+class ITerrainGenerator(Interface):
+
+    def populate(self, chunk, seed):
+        """
+        Given a chunk and a seed value, populate the chunk with terrain.
         """
 
     name = Attribute("""
