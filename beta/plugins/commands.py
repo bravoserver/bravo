22421c887fad20a1924569382f761014e8cdc000
Corbin Simpson <MostAwesomeDude@gmail.com>
2010-11-12 04:06:29 -0800

    commands: Add list.
    Heh, that last commit was all for this tiny segment of code.

--- beta/plugins/commands.py
+++ beta/plugins/commands.py
@@ -8,11 +8,23 @@ class Help(object):
 
     implements(IPlugin, ICommand)
 
-    def dispatch(self, parameters):
+    def dispatch(self, factory, parameters):
         plugins = retrieve_plugins(ICommand)
         for name, plugin in plugins.iteritems():
             print "%s" % plugin.name
 
     name = "help"
 
+class List(object):
+
+    implements(IPlugin, ICommand)
+
+    def dispatch(self, factory, parameters):
+        for player in factory.players:
+            print "%s" % player
+
+    name = "list"
+
+
 help = Help()
+list = List()
