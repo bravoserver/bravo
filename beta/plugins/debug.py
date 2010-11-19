bbc9fc9bebdb4eb9e016293c2acb1e8838960154
Corbin Simpson <MostAwesomeDude@gmail.com>
2010-11-18 22:15:30 -0800

    Add debug plugins.
    Currently, just a meliae dumper.

---
+++ beta/plugins/debug.py
@@ -0,0 +1,23 @@
+from twisted.plugin import IPlugin
+from zope.interface import implements
+
+from beta.ibeta import ICommand
+
+class Meliae(object):
+
+    implements(IPlugin, ICommand)
+
+    def dispatch(self, factory, parameters):
+        try:
+            import meliae.scanner
+            meliae.scanner.dump_all_objects(parameters)
+        except ImportError:
+            raise Exception("Couldn't import meliae!")
+        except IOError:
+            raise Exception("Couldn't save to file %s!" % parameters)
+
+        return tuple()
+
+    name = "dump_memory"
+
+meliae = Meliae()
