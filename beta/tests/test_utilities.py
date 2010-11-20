b675bcaa4a6ce54b05d099aeb1bf535df623518e
Corbin Simpson <MostAwesomeDude@gmail.com>
2010-11-19 19:28:04 -0800

    tests: More split_coords cases.

--- beta/tests/test_utilities.py
+++ beta/tests/test_utilities.py
@@ -7,6 +7,10 @@ class TestCoordHandling(unittest.TestCase):
     def test_split_coords(self):
         cases = {
             (0, 0): (0, 0, 0, 0),
+            (1, 1): (0, 1, 0, 1),
+            (16, 16): (1, 0, 1, 0),
+            (-1, -1): (-1, 15, -1, 15),
+            (-16, -16): (-1, 0, -1, 0),
         }
         for x, z in cases:
             self.assertEqual(beta.utilities.split_coords(x, z), cases[x, z])
