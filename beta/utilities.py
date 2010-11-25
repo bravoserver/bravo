7254a4793504787a45869c2b7e2b4bd3a4bd5ad6
Corbin Simpson <MostAwesomeDude@gmail.com>
2010-11-25 06:46:31 -0800

    utilities: Fix maths in doc.

--- beta/utilities.py
+++ beta/utilities.py
@@ -57,13 +57,15 @@ def rads_to_degs(radians):
     return radians * 180 / math.pi
 
 def rotated_cosine(x, y, theta, lambd):
-    """
+    r"""
     Evaluate a rotated 3D sinusoidal wave at a given point, angle, and
     wavelength.
 
     The function used is:
 
-    f(x, y) = -cos((x * cos(theta) - y * sin(theta)) / lambda) / 2 + 1
+    .. math::
+
+       f(x, y) = -\cos((x \cos\theta - y \sin\theta) / \lambda) / 2 + 1
 
     This function has a handful of useful properties; it has a local minimum
     at f(0, 0) and oscillates infinitely betwen 0 and 1.
