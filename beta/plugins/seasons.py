731ef2ebb5e474a1a5762e9981e0e6fffe438613
Corbin Simpson <MostAwesomeDude@gmail.com>
2010-11-28 12:42:38 -0800

    seasons: Snow also can't form on other things.
    Like glass. Or more snow.

--- beta/plugins/seasons.py
+++ beta/plugins/seasons.py
@@ -6,6 +6,13 @@ from zope.interface import implements
 from beta.ibeta import ISeason
 from beta.blocks import blocks
 
+snow_resistant = set([
+    blocks["air"].slot,
+    blocks["glass"].slot,
+    blocks["ice"].slot,
+    blocks["snow"].slot,
+])
+
 class Winter(object):
 
     implements(IPlugin, ISeason)
@@ -13,11 +20,10 @@ class Winter(object):
     def transform(self, chunk):
         chunk.sed(blocks["spring"].slot, blocks["ice"].slot)
 
-        # Lay snow over anything not already snowed and not ice.
+        # Lay snow over anything not already snowed and not snow-resistant.
         for x, z in product(xrange(16), xrange(16)):
             y = chunk.height_at(x, z)
-            if chunk.get_block((x, y, z)) not in (blocks["snow"].slot,
-                    blocks["ice"].slot):
+            if chunk.get_block((x, y, z)) not in snow_resistant and y < 127:
                 chunk.set_block((x, y + 1, z), blocks["snow"].slot)
 
     name = "winter"
