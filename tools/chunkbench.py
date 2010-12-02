1a50d6a9a39e50563b7810d4b52c61777cde629c
Corbin Simpson <simpsoco@osuosl.org>
2010-12-02 14:14:26 -0800

    Add chunkbench.

---
+++ tools/chunkbench.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import time
+
+from beta.chunk import Chunk
+from beta.ibeta import ITerrainGenerator
+from beta.plugin import retrieve_plugins
+
+def empty_chunk():
+
+    before = time.time()
+
+    for i in range(10):
+        Chunk(i, i)
+
+    after = time.time()
+
+    return after - before
+
+def sequential_seeded(p):
+
+    before = time.time()
+
+    for i in range(10):
+        chunk = Chunk(i, i)
+        p.populate(chunk, i)
+
+    after = time.time()
+
+    return after - before
+
+def repeated_seeds(p):
+
+    before = time.time()
+
+    for i in range(10):
+        chunk = Chunk(i, i)
+        p.populate(chunk, 0)
+
+    after = time.time()
+
+    return after - before
+
+plugins = retrieve_plugins(ITerrainGenerator)
+
+t = empty_chunk()
+print "Baseline: %f seconds" % t
+
+for name, plugin in plugins.iteritems():
+    t = sequential_seeded(plugin)
+    print "Sequential %s: %f seconds" % (name, t)
+    t = repeated_seeds(plugin)
+    print "Repeated %s: %f seconds" % (name, t)
