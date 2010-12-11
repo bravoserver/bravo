e3fa06e48634bd0b57f28dc690a807d274f004ac
Corbin Simpson <MostAwesomeDude@gmail.com>
2010-12-10 23:04:17 -0800

    chunk: Clean up imports.

--- beta/chunk.py
+++ beta/chunk.py
@@ -1,11 +1,11 @@
 import StringIO
 
-from nbt.nbt import TAG_Compound, TAG_Int, TAG_Byte_Array, TAG_Byte
+from nbt.nbt import TAG_Compound, TAG_Int
 
 from beta.alpha import Inventory
 from beta.compat import product
 from beta.packets import make_packet
-from beta.utilities import triplet_to_index, pack_nibbles, unpack_nibbles
+from beta.utilities import triplet_to_index, pack_nibbles
 
 class TileEntity(object):
 
