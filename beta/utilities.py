ff190a33b60e2c747a0d98ca9190b40fee8e4aa1
Corbin Simpson <MostAwesomeDude@gmail.com>
2010-12-11 09:06:02 -0800

    utilities: Add a timer decorator.
    Spits out average time taken to execute functions. Gonna profile with it.

--- beta/utilities.py
+++ beta/utilities.py
@@ -1,6 +1,8 @@
 from __future__ import division
 
 import math
+from functools import wraps
+from time import time
 
 # Coord handling.
 
@@ -121,3 +123,24 @@ def rotated_cosine(x, y, theta, lambd):
     """
 
     return -math.cos((x * math.cos(theta) - y * math.sin(theta)) / lambd) / 2 + 1
+
+# Decorators.
+
+timers = {}
+
+def timed(f):
+    timers[f] = (0, 0)
+    @wraps(f)
+    def deco(*args, **kwargs):
+        before = time()
+        retval = f(*args, **kwargs)
+        after = time()
+        count, average = timers[f]
+        # MMA
+        average = (9 * average + after - before) / 10
+        count += 1
+        if not count % 10:
+            print "Average time for %s: %dms" % (f, average * 1000)
+        timers[f] = (count, average)
+        return retval
+    return deco
