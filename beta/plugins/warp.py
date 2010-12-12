5b9218c75beebb10f63648c9553e986d8fbc1c52
Corbin Simpson <MostAwesomeDude@gmail.com>
2010-12-12 09:31:01 -0800

    warp: Add /home and /listwarps.
    Still needs more work to conform to hey0, but the groundwork's there. If only
    it weren't so ugly.

--- beta/plugins/warp.py
+++ beta/plugins/warp.py
@@ -8,6 +8,17 @@ from beta.ibeta import IChatCommand, IConsoleCommand
 
 csv.register_dialect("hey0", delimiter=":")
 
+homes = {}
+
+for line in csv.reader(open("world/homes.txt", "rb"), "hey0"):
+    name, x, y, z, theta, pitch = line[:6]
+    x = float(x)
+    y = float(y)
+    z = float(z)
+    theta = radians(float(theta))
+    pitch = float(pitch)
+    homes[name] = (x, y, z, theta, pitch)
+
 warps = {}
 
 for line in csv.reader(open("world/warps.txt", "rb"), "hey0"):
@@ -18,6 +29,32 @@ for line in csv.reader(open("world/warps.txt", "rb"), "hey0"):
     theta = radians(float(theta))
     pitch = float(pitch)
     warps[name] = (x, y, z, theta, pitch)
+
+class Home(object):
+
+    implements(IPlugin, IChatCommand, IConsoleCommand)
+
+    def chat_command(self, factory, username, parameters):
+        protocol = factory.players[username]
+        l = protocol.player.location
+        if username in warps:
+            yield "Teleporting %s home" % username
+            (l.x, l.y, l.z, l.theta, l.pitch) = homes[username]
+        else:
+            yield "Teleporting %s to spawn" % username
+            l.x, l.y, l.z = factory.world.spawn
+            l.theta, l.pitch = 0, 0
+        protocol.send_initial_chunk_and_location()
+        yield "Teleportation successful!"
+
+    def console_command(self, factory, parameters):
+        for i in self.chat_command(factory, parameters[0], parameters[1:]):
+            yield i
+
+    name = "home"
+    aliases = tuple()
+    usage = ""
+    info = "Warps player home"
 
 class Warp(object):
 
@@ -52,4 +89,28 @@ class Warp(object):
     usage = "<location>"
     info = "Warps player to a location"
 
+class ListWarps(object):
+
+    implements(IPlugin, IChatCommand, IConsoleCommand)
+
+    def dispatch(self):
+        yield "Warp locations:"
+        for key in sorted(warps.iterkeys()):
+            yield "~ %s" % key
+
+    def chat_command(self, factory, username, parameters):
+        for i in self.dispatch():
+            yield i
+
+    def console_command(self, factory, parameters):
+        for i in self.dispatch():
+            yield i
+
+    name = "listwarps"
+    aliases = tuple()
+    usage = ""
+    info = "List warps"
+
+home = Home()
 warp = Warp()
+listwarps = ListWarps()
