edc659eca7a48ebee081f03da4ff47d8ef986d6e
Corbin Simpson <MostAwesomeDude@gmail.com>
2010-12-19 07:50:29 -0800

    Add setup.py and MANIFEST.in.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Beta",
+    version="0.9",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "Twisted",
+    ],
+    author="Corbin Simpson",
+    author_email="MostAwesomeDude@gmail.com",
+    description="Minecraft server and utilities",
+    long_description=open("README.rst").read(),
+    license="MIT/X11",
+    url="http://github.com/MostAwesomeDude/beta",
+)
