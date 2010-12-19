d3c2e75a6b2a889b53441a8a141333675cbdbd31
Corbin Simpson <MostAwesomeDude@gmail.com>
2010-12-18 22:53:10 -0800

    tests: Fix tests, remove useless tests.

--- beta/tests/test_utilities.py
+++ beta/tests/test_utilities.py
@@ -1,5 +1,7 @@
 import unittest
 
+from numpy import array
+
 import beta.utilities
 
 class TestCoordHandling(unittest.TestCase):
@@ -15,27 +17,6 @@ class TestCoordHandling(unittest.TestCase):
         for x, z in cases:
             self.assertEqual(beta.utilities.split_coords(x, z), cases[x, z])
 
-    def test_triplet_to_index(self):
-        self.assertEqual(0, beta.utilities.triplet_to_index((0, 0, 0)))
-
-        cases = [
-            (17, 0, 0),
-            (-1, 255, -1),
-            (15, 255, 15),
-            (-1, -1, -1),
-        ]
-        for triplet in cases:
-            self.assertRaises(Exception, beta.utilities.triplet_to_index,
-                triplet)
-
-    def test_index_to_triplet(self):
-        self.assertEqual(0, beta.utilities.triplet_to_index((0, 0, 0)))
-
-        cases = [-1, 65536]
-        for index in cases:
-            self.assertRaises(Exception, beta.utilities.triplet_to_index,
-                index)
-
 class TestBitTwiddling(unittest.TestCase):
 
     def test_unpack_nibbles(self):
@@ -44,7 +25,9 @@ class TestBitTwiddling(unittest.TestCase):
             [6, 14, 6, 9, 6, 2, 6, 2, 6, 12, 6, 5, 7, 3])
 
     def test_pack_nibbles(self):
-        self.assertEqual(beta.utilities.pack_nibbles([6, 1]), ["a"])
+        self.assertEqual(list(beta.utilities.pack_nibbles(array([6, 1]))),
+            ["a"])
         self.assertEqual(
-            beta.utilities.pack_nibbles([6, 14, 6, 9, 6, 2, 7, 3]),
+            list(beta.utilities.pack_nibbles(
+                    array([6, 14, 6, 9, 6, 2, 7, 3]))),
             list("nibs"))
