6f870c7393102ea4c5b3a3e6d6429f539b7d0cda
Corbin Simpson <MostAwesomeDude@gmail.com>
2010-12-21 02:12:34 -0800

    main: Log. Everything.

--- beta/main.py
+++ beta/main.py
@@ -4,11 +4,14 @@ import traceback
 
 from twisted.internet import reactor
 from twisted.internet.stdio import StandardIO
+from twisted.python import log
 
 from beta.stdio import runWithProtocol, stopConsole
 from beta.factory import BetaFactory
 
 def main():
+    log.startLogging(open("beta.log", "w"))
+
     # The try/except is absolutely essential. No matter what, we *must*
     # restore the console if we are able to do so, especially if we're gonna
     # die and vomit out a traceback. On the other hand, we only want to stop
