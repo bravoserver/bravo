d4ed07afc33d6a23a1c52b7df9120489883408fb
Corbin Simpson <MostAwesomeDude@gmail.com>
2010-12-27 22:39:39 -0800

    Add RPC layout for Ampoule.

---
+++ bravo/remote.py
@@ -0,0 +1,58 @@
+from ampoule import AMPChild
+import ampoule.pool
+
+from twisted.protocols.amp import Command, Integer, String
+
+from bravo.chunk import Chunk
+from bravo.ibravo import ITerrainGenerator
+from bravo.plugin import retrieve_plugins
+
+class MakeChunk(Command):
+    arguments = [
+        ("x", Integer()),
+        ("z", Integer()),
+        ("seed", Integer()),
+        ("generators", String()),
+    ]
+    response = [
+        ("blocks", String()),
+        ("metadata", String()),
+        ("skylight", String()),
+        ("blocklight", String()),
+        ("heightmap", String()),
+    ]
+    errors = {
+        Exception: "Exception",
+    }
+
+class Slave(AMPChild):
+    """
+    Process-based peon for processing and populating.
+    """
+
+    def make_chunk(self, x, z, seed, generators):
+        """
+        Create a chunk using the given parameters.
+        """
+
+        plugins = retrieve_plugins(ITerrainGenerator)
+        stages = [plugins[g] for g in generators.split(",")]
+
+        chunk = Chunk(x, z)
+
+        for stage in stages:
+            stage.populate(chunk, seed)
+
+        return {
+            "blocks": "".join(chr(i) for i in chunk.blocks.ravel()),
+            "metadata": "".join(chr(i) for i in chunk.metadata.ravel()),
+            "skylight": "".join(chr(i) for i in chunk.skylight.ravel()),
+            "blocklight": "".join(chr(i) for i in chunk.lightmap.ravel()),
+            "heightmap": "".join(chr(i) for i in chunk.heightmap.ravel()),
+        }
+
+    MakeChunk.responder(make_chunk)
+
+ampoule.pool.pp = ampoule.pool.ProcessPool(
+    ampChild=Slave,
+)
