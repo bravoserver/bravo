4f6c89cdcc55c1981cb1f3cfc1f9bb6140069fd5
Corbin Simpson <simpsoco@osuosl.org>
2010-12-30 17:09:00 -0800

    tests: Add another world test.

--- bravo/tests/test_world.py
+++ bravo/tests/test_world.py
@@ -36,3 +36,5 @@ class TestWorldUtilities(unittest.TestCase):
     def test_names_for_chunk(self):
         self.assertEqual(bravo.world.names_for_chunk(-13, 44),
             ("1f", "18", "c.-d.18.dat"))
+        self.assertEqual(bravo.world.names_for_chunk(-259, 266),
+            ("1p", "a", "c.-77.7e.dat"))
