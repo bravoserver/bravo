cc76690b79b486ed078627facc50945cab6cb6ee
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-01-08 23:35:10 -0800

    recipes: Add sticks.

--- bravo/plugins/recipes.py
+++ bravo/plugins/recipes.py
@@ -1,7 +1,7 @@
 from twisted.plugin import IPlugin
 from zope.interface import implements
 
-from bravo.blocks import blocks
+from bravo.blocks import blocks, items
 from bravo.ibravo import IRecipe
 
 class Wood(object):
@@ -17,5 +17,20 @@ class Wood(object):
     )
 
     provides = (blocks["wood"].slot, 4)
+
+class Sticks(object):
+
+    implements(IPlugin, IRecipe)
+
+    name = "sticks"
+
+    dimensions = (1, 2)
+
+    recipe = (
+        (blocks["wood"].slot, 1),
+        (blocks["wood"].slot, 1),
+    )
+
+    provides = (items["stick"].slot, 4)
 
 wood = Wood()
