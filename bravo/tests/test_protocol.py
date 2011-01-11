87f8f2630e0898fae3a0061871f16fc645ab2183
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-01-10 18:07:05 -0800

    tests: Add trivial protocol test.
    Might expand on this later. We'll see.

---
+++ bravo/tests/test_protocol.py
@@ -0,0 +1,11 @@
+import unittest
+
+import bravo.protocol
+
+class TestBetaProtocol(unittest.TestCase):
+
+    def setUp(self):
+        self.p = bravo.protocol.BetaProtocol()
+
+    def test_trivial(self):
+        pass
