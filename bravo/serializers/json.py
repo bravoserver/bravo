5fe9e82fbfc4143e4eae96ceb44eea6e345c5f76
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-01-13 04:58:48 -0800

    Add JSON serializer.

---
+++ bravo/serializers/json.py
@@ -0,0 +1,181 @@
+from itertools import chain
+
+try:
+    from json import loads, dumps
+except ImportError:
+    try:
+        from simplejson import loads, dumps
+    except ImportError:
+        raise ImportError("Couldn't find a JSON library!")
+
+from bravo.utilities import unpack_nibbles, pack_nibbles
+
+class InventorySerializer(object):
+
+    def load_from_tag(inventory, l):
+
+        items = [None] * len(inventory)
+
+        for item in l:
+            slot = item["Slot"]
+            items[slot] = item["id"], item["Damage"], item["Count"]
+
+        inventory.load_from_list(items)
+
+    def save_to_tag(inventory):
+
+        l = []
+
+        for i, item in enumerate(chain(inventory.crafted,
+            inventory.crafting, inventory.armor, inventory.storage,
+            inventory.holdables)):
+            if item is not None:
+                id, damage, count = item
+                d = {
+                    "id": id,
+                    "Damage": damage,
+                    "Count": count,
+                    "Slot": i,
+                }
+                l.append(d)
+
+        return l
+
+class ChestSerializer(object):
+
+    def load_from_tag(chest, d):
+
+        chest.x = d["x"]
+        chest.y = d["y"]
+        chest.z = d["z"]
+
+        chest.inventory.load_from_tag(d["Items"])
+
+    def save_to_tag(chest):
+
+        d = {
+            "id": "Chest",
+            "x": chest.x,
+            "y": chest.y,
+            "z": chest.z,
+            "Items": chest.inventory.save_to_tag(),
+        }
+
+        return d
+
+class SignSerializer(object):
+
+    def load_from_tag(sign, d):
+
+        sign.x = d["x"]
+        sign.y = d["y"]
+        sign.z = d["z"]
+
+        sign.text1 = d["Text1"]
+        sign.text2 = d["Text2"]
+        sign.text3 = d["Text3"]
+        sign.text4 = d["Text4"]
+
+    def save_to_tag(sign):
+
+        d = {
+            "id": "Sign",
+            "x": sign.x,
+            "y": sign.y,
+            "z": sign.z,
+            "Text1": sign.text1,
+            "Text2": sign.text2,
+            "Text3": sign.text3,
+            "Text4": sign.text4,
+        }
+
+        return d
+
+class ChunkSerializer(object):
+
+    def load_from_tag(chunk, d):
+
+        level = d["Level"]
+
+        chunk.blocks.ravel()[:] = [ord(i) for i in level["Blocks"]]
+        chunk.heightmap.ravel()[:] = [ord(i) for i in level["HeightMap"]]
+        chunk.blocklight.ravel()[:] = unpack_nibbles(level["BlockLight"])
+        chunk.metadata.ravel()[:] = unpack_nibbles(level["Data"])
+        chunk.skylight.ravel()[:] = unpack_nibbles(level["SkyLight"])
+
+        chunk.populated = level["TerrainPopulated"]
+
+        if "TileEntities" in level:
+            for t in level["TileEntities"]:
+                try:
+                    tile = chunk.known_tile_entities[t["id"]]()
+                    tile.load_from_tag(t)
+                    chunk.tiles[tile.x, tile.y, tile.z] = tile
+                except:
+                    print "Unknown tile entity %s" % t["id"]
+
+        chunk.dirty = not chunk.populated
+
+    def save_to_tag(chunk):
+
+        d = {
+            "Level:": {
+                "Blocks": "".join(chr(i) for i in chunk.blocks.ravel()),
+                "HeightMap": "".join(chr(i) for i in chunk.heightmap.ravel()),
+                "BlockLight": "".join(
+                    pack_nibbles(chunk.blocklight)
+                ),
+                "Data": "".join(pack_nibbles(chunk.metadata)),
+                "SkyLight": "".join(pack_nibbles(chunk.skylight)),
+                "TerrainPopulated": chunk.populated,
+                "TileEntities": [tile.save_to_tag()
+                    for tile in chunk.tiles.itervalues()
+                ],
+            }
+        }
+
+        return d
+
+class LevelSerializer(object):
+
+    def load_from_tag(level, d):
+
+        level.spawn = (d["Data"]["SpawnX"],
+            d["Data"]["SpawnY"],
+            d["Data"]["SpawnZ"])
+
+        level.seed = d["Data"]["RandomSeed"]
+
+    def save_to_tag(level):
+
+        d = {
+            "Data": {
+                "RandomSeed": level.seed,
+                "SpawnX": level.spawn[0],
+                "SpawnY": level.spawn[1],
+                "SpawnZ": level.spawn[2],
+            }
+        }
+
+        return d
+
+class PlayerSerializer(object):
+
+    def load_from_tag(player, d):
+
+        player.location.x, player.location.y, player.location.z = d["Pos"]
+
+        player.location.yaw, player.location.pitch = d["Rotation"]
+
+        if "Inventory" in d:
+            player.inventory.load_from_tag(d["Inventory"])
+
+    def save_to_tag(player):
+
+        d = {
+            "Pos": (player.location.x, player.location.y, player.location.z),
+            "Rotation": (player.location.yaw, player.location.pitch),
+            "Inventory": player.inventory.save_to_tag(),
+        }
+
+        return d
