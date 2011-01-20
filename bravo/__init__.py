b9c82f696c78650ae36de444feb4ec43fc08e3df
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-01-19 23:49:02 -0800

    Bump to 1.1.

--- bravo/__init__.py
+++ bravo/__init__.py
@@ -1,1 +1,1 @@
-version = u"1.0"
+version = u"1.1"
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="Bravo",
-    version="1.0",
+    version="1.1",
     packages=["bravo", "twisted.plugins"],
     install_requires=[
         "numpy",
