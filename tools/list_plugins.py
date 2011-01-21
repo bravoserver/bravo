4ed1c1db2014a73591300ac55999f3803c34a288
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-01-21 15:35:12 -0800

    tools: More list_plugins.

--- tools/list_plugins.py
+++ tools/list_plugins.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
 from bravo.ibravo import IAuthenticator, ITerrainGenerator, IBuildHook
-from bravo.ibravo import IDigHook, ISeason
+from bravo.ibravo import IDigHook, IRecipe, ISeason
 from bravo.plugin import retrieve_plugins
 
 for interface in (IAuthenticator, IBuildHook, IDigHook, ISeason,
-    ITerrainGenerator):
+    ITerrainGenerator, IRecipe):
     print "Interface: %s" % interface
-    print "Available hooks:"
-    for name, plugin in retrieve_plugins(interface).items():
+    print "Number of plugins: %d" % len(retrieve_plugins(interface))
+    print "Available plugins:"
+    for name, plugin in sorted(retrieve_plugins(interface).items()):
         print " ~ %s" % name
