802078a44d8ed66751de9f72d6941666e44b696b
Corbin Simpson <simpsoco@osuosl.org>
2011-02-18 18:28:36 -0800

    service: Add more robust dynamic service management.

--- bravo/service.py
+++ bravo/service.py
@@ -16,28 +16,43 @@ class BetaProxyFactory(Factory):
         self.name = name
         self.port = configuration.getint("infiniproxy %s" % name, "port")
 
-service = MultiService()
+class BravoService(MultiService):
+
+    def __init__(self):
+        MultiService.__init__(self)
+
+        # Start up our AMP RPC.
+        self.amp = TCPServer(25600, ConsoleRPCFactory(self))
+        MultiService.addService(self, self.amp)
+
+    def addService(self, service):
+        MultiService.addService(self, service)
+
+    def removeService(self, service):
+        MultiService.removeService(self, service)
+
+    def configure_services(self, configuration):
+        for section in configuration.sections():
+            if section.startswith("world "):
+                factory = BravoFactory(section[6:])
+                server = TCPServer(factory.port, factory,
+                    interface=factory.interface)
+                self.addService(server)
+            elif section.startswith("irc "):
+                factory = BravoIRC(worlds, section[4:])
+                self.addService(TCPClient(factory.host, factory.port,
+                    factory))
+            elif section.startswith("infiniproxy "):
+                factory = BetaProxyFactory(section[12:])
+                self.addService(TCPServer(factory.port, factory))
+            elif section.startswith("infininode "):
+                factory = InfiniNodeFactory(section[11:])
+                self.addService(TCPServer(factory.port, factory))
+
+
+service = BravoService()
 
 worlds = []
-for section in configuration.sections():
-    if section.startswith("world "):
-        factory = BravoFactory(section[6:])
-        server = TCPServer(factory.port, factory, interface=factory.interface)
-        server.setServiceParent(service)
-        worlds.append(factory)
-    elif section.startswith("irc "):
-        factory = BravoIRC(worlds, section[4:])
-        TCPClient(factory.host, factory.port,
-            factory).setServiceParent(service)
-    elif section.startswith("infiniproxy "):
-        factory = BetaProxyFactory(section[12:])
-        TCPServer(factory.port, factory).setServiceParent(service)
-    elif section.startswith("infininode "):
-        factory = InfiniNodeFactory(section[11:])
-        TCPServer(factory.port, factory).setServiceParent(service)
-
-# Start up our AMP.
-TCPServer(25600, ConsoleRPCFactory(worlds)).setServiceParent(service)
 
 application = Application("Bravo")
 service.setServiceParent(application)
