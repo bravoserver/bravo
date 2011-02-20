d7fcbf8d2d348cc3a5ccf9cab2a481fd6c98c366
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-02-19 19:31:50 -0800

    tests: Move generator tests to plugins.
    And add the skip-test functionality.

---
+++ bravo/tests/plugins/test_generators.py
@@ -0,0 +1,73 @@
+import unittest
+
+import bravo.blocks
+import bravo.chunk
+import bravo.ibravo
+import bravo.plugin
+
+class TestGenerators(unittest.TestCase):
+
+    def setUp(self):
+        self.chunk = bravo.chunk.Chunk(0, 0)
+
+        self.p = bravo.plugin.retrieve_plugins(bravo.ibravo.ITerrainGenerator)
+
+    def test_trivial(self):
+        pass
+
+    def test_boring(self):
+        if "boring" not in self.p:
+            raise unittest.SkipTest("plugin not present")
+
+        plugin = self.p["boring"]
+
+        plugin.populate(self.chunk, 0)
+        for x, y, z in bravo.compat.product(xrange(16), xrange(128),
+            xrange(16)):
+            if y < 64:
+                self.assertEqual(self.chunk.get_block((x, y, z)),
+                    bravo.blocks.blocks["stone"].slot)
+            else:
+                self.assertEqual(self.chunk.get_block((x, y, z)),
+                    bravo.blocks.blocks["air"].slot)
+
+    def test_beaches_range(self):
+        if "beaches" not in self.p:
+            raise unittest.SkipTest("plugin not present")
+
+        plugin = self.p["beaches"]
+
+        # Prepare chunk.
+        for i in range(5):
+            self.chunk.blocks[i, i, 61 + i] = bravo.blocks.blocks["dirt"].slot
+
+        plugin.populate(self.chunk, 0)
+        for i in range(5):
+            self.assertEqual(self.chunk.get_block((i, 61 + i, i)),
+                bravo.blocks.blocks["sand"].slot,
+                "%d, %d, %d is wrong" % (i, 61 + i, i))
+
+    def test_beaches_immersed(self):
+        """
+        Test that beaches still generate properly around pre-existing water
+        tables.
+
+        This test is meant to ensure that the order of beaches and watertable
+        does not matter.
+        """
+
+        if "beaches" not in self.p:
+            raise unittest.SkipTest("plugin not present")
+
+        plugin = self.p["beaches"]
+
+        # Prepare chunk.
+        self.chunk.blocks[:, :, 60:64].fill(bravo.blocks.blocks["spring"].slot)
+        for i in range(5):
+            self.chunk.blocks[i, i, 61 + i] = bravo.blocks.blocks["dirt"].slot
+
+        plugin.populate(self.chunk, 0)
+        for i in range(5):
+            self.assertEqual(self.chunk.get_block((i, 61 + i, i)),
+                bravo.blocks.blocks["sand"].slot,
+                "%d, %d, %d is wrong" % (i, 61 + i, i))
