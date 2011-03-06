0643ca326806fe9760d0df5b49c020afb04e7ae8
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-03-06 00:45:03 -0800

    tests: Fix factory tests.

--- bravo/tests/factories/test_beta.py
+++ bravo/tests/factories/test_beta.py
@@ -14,20 +14,22 @@ class TestBravoFactory(unittest.TestCase):
         self.name = "unittest"
 
         bravo.config.configuration.add_section("world unittest")
-        bravo.config.configuration.set("world unittest", "url", "file://%s" % self.d)
+        bravo.config.configuration.set("world unittest", "url",
+            "file://%s" % self.d)
         bravo.config.configuration.set("world unittest", "serializer",
             "alpha")
         bravo.config.configuration.set("world unittest", "port", "0")
+        bravo.config.configuration.set("world unittest", "seasons", "")
 
         self.f = bravo.factories.beta.BravoFactory(self.name)
 
     def tearDown(self):
+        bravo.config.configuration.remove_section("world unittest")
+
         self.f.world.chunk_management_loop.stop()
         self.f.time_loop.stop()
 
         shutil.rmtree(self.d)
-
-        bravo.config.configuration.remove_section("world unittest")
 
     def test_trivial(self):
         pass
