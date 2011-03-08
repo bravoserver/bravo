50351cea8f749df1e1074a93de8abaff98178c03
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-03-08 13:23:39 -0800

    tests: Add water tests.

---
+++ bravo/tests/plugins/test_physics.py
@@ -0,0 +1,82 @@
+import shutil
+import tempfile
+
+from twisted.trial import unittest
+
+import bravo.blocks
+import bravo.config
+import bravo.ibravo
+import bravo.plugin
+import bravo.world
+
+class WaterMockFactory(object):
+
+    def flush_chunk(self, chunk):
+        pass
+
+class TestWater(unittest.TestCase):
+
+    def setUp(self):
+        # Using build hook to grab the plugin, but dig hook should work as
+        # well.
+        self.p = bravo.plugin.retrieve_plugins(bravo.ibravo.IBuildHook)
+
+        if "water" not in self.p:
+            raise unittest.SkipTest("Plugin not present")
+
+        self.hook = self.p["water"]
+
+        # Set up world.
+        self.name = "unittest"
+        self.d = tempfile.mkdtemp()
+
+        bravo.config.configuration.add_section("world unittest")
+        bravo.config.configuration.set("world unittest", "url",
+            "file://%s" % self.d)
+        bravo.config.configuration.set("world unittest", "serializer",
+            "alpha")
+
+        self.w = bravo.world.World(self.name)
+        self.w.pipeline = []
+
+        # And finally the mock factory.
+        self.f = WaterMockFactory()
+        self.f.world = self.w
+
+    def tearDown(self):
+        if self.w.chunk_management_loop.running:
+            self.w.chunk_management_loop.stop()
+        del self.w
+
+        shutil.rmtree(self.d)
+        bravo.config.configuration.remove_section("world unittest")
+
+    def test_trivial(self):
+        pass
+
+    def test_zero_y(self):
+        """
+        Double-check that water placed on the very bottom of the world doesn't
+        cause internal errors.
+        """
+
+        self.w.set_block((0, 0, 0), bravo.blocks.blocks["spring"].slot)
+        self.hook.tracked.add((self.f, 0, 0, 0))
+
+        # Tight-loop run the hook to equilibrium; if any exceptions happen,
+        # they will bubble up.
+        while self.hook.tracked:
+            self.hook.process()
+
+    def test_spring_spread(self):
+        self.w.set_block((0, 0, 0), bravo.blocks.blocks["spring"].slot)
+        self.hook.tracked.add((self.f, 0, 0, 0))
+
+        # Tight-loop run the hook to equilibrium.
+        while self.hook.tracked:
+            self.hook.process()
+
+        for coords in ((1, 0, 0), (-1, 0, 0), (0, 0, 1), (0, 0, -1)):
+            self.assertEqual(self.w.get_block(coords),
+                bravo.blocks.blocks["water"].slot)
+            self.assertEqual(self.w.get_metadata(coords), 0x0)
