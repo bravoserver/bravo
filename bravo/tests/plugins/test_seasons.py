e404a6e85f254779af9b85b89cce59f3ce97978f
Reiner Gerecke <mr.squidd@gmail.com>
2011-03-20 19:17:15 +0100

    tests: Add spring season test.

--- bravo/tests/plugins/test_seasons.py
+++ bravo/tests/plugins/test_seasons.py
@@ -74,3 +74,29 @@ class TestWinter(unittest.TestCase):
             bravo.blocks.blocks["snow"].slot)
         self.assertNotEqual(self.c.get_block((0, 2, 0)),
             bravo.blocks.blocks["snow"].slot)
+
+class Spring(unittest.TestCase):
+    
+    def setUp(self):
+        self.p = bravo.plugin.retrieve_plugins(bravo.ibravo.ISeason)
+
+        if "spring" not in self.p:
+            raise unittest.SkipTest("Plugin not present")
+
+        self.hook = self.p["spring"]
+        self.c = bravo.chunk.Chunk(0, 0)
+
+    def test_trivial(self):
+        pass
+
+    def test_ice_to_spring(self):
+        self.c.set_block((0, 0, 0), bravo.blocks.blocks["ice"].slot)
+        self.hook.transform(self.c)
+        self.assertEqual(self.c.get_block((0, 0, 0)),
+            bravo.blocks.blocks["spring"].slot)
+
+    def test_snow_to_air(self):
+        self.c.set_block((0, 0, 0), bravo.blocks.blocks["snow"].slot)
+        self.hook.transform(self.c)
+        self.assertEqual(self.c.get_block((0, 0, 0)),
+            bravo.blocks.blocks["air"].slot)
