929deae179fd6dc597265f0121a3cc6511084a44
Corbin Simpson <simpsoco@osuosl.org>
2011-03-28 10:43:57 -0700

    tests: Test for /give patch.

--- bravo/tests/plugins/test_commands.py
+++ bravo/tests/plugins/test_commands.py
@@ -19,6 +19,9 @@ class CommandsMockFactory(object):
             "unittest": CommandsMockProtocol(),
         }
 
+    def give(self, coords, block, count):
+        pass
+
 class TestGetpos(unittest.TestCase):
 
     def setUp(self):
@@ -38,3 +41,31 @@ class TestGetpos(unittest.TestCase):
         self.assertTrue(retval)
         l = list(retval)
         self.assertEqual(len(l), 1)
+
+class TestGive(unittest.TestCase):
+
+    def setUp(self):
+        self.p = bravo.plugin.retrieve_plugins(bravo.ibravo.IChatCommand)
+
+        if "getpos" not in self.p:
+            raise unittest.SkipTest("Plugin not present")
+
+        self.hook = self.p["getpos"]
+
+    def test_trivial(self):
+        pass
+
+    def test_no_parameters(self):
+        """
+        With no parameters, the command shouldn't call factory.give().
+        """
+
+        factory = CommandsMockFactory()
+        called = [False]
+        def cb(a, b, c):
+            called[0] = True
+        self.patch(factory, "give", cb)
+
+        self.hook.chat_command(factory, "unittest", [])
+
+        self.assertFalse(called[0])
