982a6efcde76fbcc162256f7cb9d0531c62b1660
Corbin Simpson <simpsoco@osuosl.org>
2011-04-04 18:38:51 -0700

    web: Switch to Twisted templates.
    Requires Twisted 11.0, oh noes!

--- bravo/web.py
+++ bravo/web.py
@@ -1,7 +1,25 @@
 from twisted.web.resource import Resource
-from twisted.web.server import Site
+from twisted.web.server import Site, NOT_DONE_YET
+from twisted.web.template import flattenString, renderer, Element, XMLString
 
 from bravo import version
+
+root_template = """
+<html xmlns:t="http://twistedmatrix.com/ns/twisted.web.template/0.1">
+<head>
+    <title t:render="title" />
+</head>
+<h1 t:render="title" />
+</html>
+"""
+
+class BravoElement(Element):
+
+    loader = XMLString(root_template)
+
+    @renderer
+    def title(self, request, tag):
+        return tag("Bravo %s" % version)
 
 class BravoResource(Resource):
 
@@ -11,7 +29,16 @@ class BravoResource(Resource):
         self.services = services
 
     def render_GET(self, request):
-        response = u"Bravo %s" % version
+        d = flattenString(request, BravoElement())
+        def complete_request(html):
+            request.write(html)
+            request.finish()
+        d.addCallback(complete_request)
+        return NOT_DONE_YET
+
+        response = u"<h1>Bravo %s</h1>" % version
+        for name, service in self.services.iteritems():
+            response += u"<h2>%s</h2><p>%s</p>" % (name, type(service))
         return response.encode("utf8")
 
 def bravo_site(services):
