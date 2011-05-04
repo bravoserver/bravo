75c04376b65c8f1b8e7267ca708bf1879ec72e89
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-05-04 00:21:23 -0700

    tests: Remove unused tests; add dig policy tests.
    Out with the old, in with the new.

---
+++ bravo/tests/policy/test_dig.py
@@ -0,0 +1,18 @@
+import unittest
+
+from bravo.blocks import blocks
+from bravo.policy.dig import dig_policies
+
+class TestNotchyDigPolicy(unittest.TestCase):
+
+    def setUp(self):
+        self.p = dig_policies["notchy"]
+
+    def test_trivial(self):
+        pass
+
+    def test_snow_1ko(self):
+        self.assertTrue(self.p.is_1ko(blocks["snow"].slot))
+
+    def test_dirt_bare(self):
+        self.assertAlmostEqual(self.p.dig_time(blocks["dirt"].slot), 0.75)
