1809b44ec3d28156e5d6f1e74192ef90a37e6b1a
Olav Andreas Lindekleiv <olalinde@gmail.com>
2011-05-08 20:54:42 +0200

    Added a fertilizer plugin that lets you use bone meal to instantly grow saplings to trees, and added it to the example config file.

---
+++ bravo/plugins/fertilizer.py
@@ -0,0 +1,55 @@
+from twisted.internet.defer import inlineCallbacks, returnValue
+from zope.interface import implements
+
+from bravo.blocks import blocks, items
+from bravo.ibravo import IBuildHook
+from bravo.terrain.trees import ConeTree, NormalTree, RoundTree
+
+class Fertilizer(object):
+    """
+    Allows you to use bone meal to fertilize trees,
+    and make them grow up instantly.
+    """
+
+    implements(IBuildHook)
+
+    def __init__(self):
+        self.trees = [
+            NormalTree,
+            ConeTree,
+            RoundTree,
+            NormalTree,
+        ]
+
+    @inlineCallbacks
+    def build_hook(self, factory, player, builddata):
+        item, metadata, x, y, z, face = builddata
+
+        # Make sure we're using a bone meal.
+        # TODO: We need to check metadata, but it's not implemented yet.
+        # Now all dyes will work as a fertilizer.
+        if item.slot == items["bone-meal"].slot:
+            # Find the block we're aiming for.
+            sapling_block = yield factory.world.get_block((x,y,z))
+            if sapling_block == blocks["sapling"].slot:
+                # Make sure we can remove it from the inventory.
+                if not player.inventory.consume(items["bone-meal"].key, player.equipped):
+                    returnValue((False, builddata))
+
+                # Select correct treee and coordinates, then build tree.
+                tree = self.trees[metadata % 4](pos=(x, y, z))
+                tree.prepare(factory.world)
+                tree.make_trunk(factory.world)
+                tree.make_foliage(factory.world)
+                # We can't easily tell how many chunks were modified, so we have
+                # to flush all of them.
+                factory.flush_all_chunks()
+
+        returnValue((True, builddata))
+
+    name = "fertilizer"
+
+    before = tuple()
+    after = tuple()
+
+fertilizer = Fertilizer()
