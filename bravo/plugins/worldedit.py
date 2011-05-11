661b142b2316c9c99c32c36c8dc2084043df40a4
Christophe 'Pwipwi' Eymard <christophe.eymard@gmail.com>
2011-05-11 21:59:23 +0200

    Added the /jumpto command.

---
+++ bravo/plugins/worldedit.py
@@ -0,0 +1,96 @@
+from math import pi, cos, sin
+
+from StringIO import StringIO
+
+from zope.interface import implements
+
+from bravo.ibravo import IChatCommand, IConsoleCommand
+from bravo.blocks import blocks
+
+from bravo.utilities.geometry import gen_line_covered
+
+"""
+    This plugin shall mimick the worldedit plugin for the original minecraft_server.jar.
+"""
+
+empty_blocks_names = (
+    "air", "snow", "sapling", "water", "spring",
+    "flower", "rose", "brown-mushroom", "red-mushroom",
+    "torch", "fire", "redstone-wire", "crops", "soil", "signpost",
+    "wooden-door", "iron-door", "wall-sign", "lever",
+    "stone-plate", "wooden-plate", "sugar-cane", "fence", "portal",
+    "redstone-repeater-on", "redstone-repeater-off",
+)
+
+empty_blocks = []
+for name in empty_blocks_names:
+    empty_blocks.append (blocks[name].slot)
+
+class _Point (object):
+    """ Small temporary class to hold a 3d point.
+    """
+    def __init__ (self, vec): self.x, self.y, self.z = vec
+
+class Jumpto(object):
+    """
+        Teleport the player to the block he's looking at.
+    """
+
+    implements(IChatCommand, IConsoleCommand)
+
+    def chat_command(self, factory, username, parameters):
+        yield "Trying to Jump..."
+
+        protocol = factory.protocols[username] # Our player object
+
+        l = protocol.player.location
+        # Viewport and player location are not very adapted to the coordinate system :
+        # Blocks are not aligned on their centers. To be standing in the middle of a block,
+        # you have to be at +0.5 +0.5 on x and z positions.
+        o = _Point ( (l.x, l.y + 1.6, l.z) )
+
+        # x = r sinq cosf,     y = r sinq sinf,     z = r cosq,
+        distant_point = _Point ( (-1 * 220 * cos (l.phi) * sin (l.theta), -1 * 220 * sin (l.phi), 220 * cos (l.theta) * cos (l.phi) ) )
+        distant_point.x += o.x
+        distant_point.y += o.y
+        distant_point.z += o.z
+
+        world = factory.world
+        dest = None
+        for point in gen_line_covered (o, distant_point):
+            block = world.get_block ( point )
+            if block.result not in empty_blocks: # it's not air !!
+                dest = [point[0], point[1], point[2]]
+                break
+
+        if not dest:
+            yield "Could not find a suitable destination..."
+            return
+
+        # Now we find the first vertical space that can host us.
+        current_block = -1
+        prev_block = -1
+        while True: # Should include non block as well !
+            prev_block = current_block
+            dest[1] += 1
+            if dest[1] >= 127:
+                dest[1] += 1
+                break
+            current_block = world.get_block (dest).result
+            if current_block in empty_blocks and prev_block in empty_blocks:
+                break
+
+        l.x, l.y, l.z = dest[0] + 0.5, dest[1], dest[2] + 0.5
+        protocol.send_initial_chunk_and_location()
+        yield "*Poof*"
+
+    def console_command(self, factory, parameters):
+        for i in self.chat_command(factory, parameters[0], parameters[1:]):
+            yield i
+
+    name = "jumpto"
+    aliases = tuple()
+    usage = "<name>"
+    info = "Teleports you where you're looking at."
+
+jumpto = Jumpto ()
---
+++ bravo/utilities/geometry.py
@@ -0,0 +1,79 @@
+
+
+def gen_line_simple (point1, point2):
+    """ Generalization of Bresenham's line algorithm in 3d.
+
+        It yields blocks coordinates along a line that is at all times only
+        one block in width.
+
+        It is a simplified version that does nothing fancy with integers or such : all
+        computation is done on float (maybe it should be changed?)
+    """
+    tx, ty, tz = point1.x, point1.y, point1.z # t is for temporary
+    rx, ry, rz = int (tx), int (ty), int (tz) # r is for rounded
+    ox, oy, oz = point2.x, point2.y, point2.z # o is for objective
+
+    dx = ox - tx
+    dy = oy - ty
+    dz = oz - tz
+
+    largest = max (abs (dx), abs (dy), abs (dz)) * 1. # We want a float
+    dx, dy, dz = dx / largest, dy / largest, dz / largest # We make a vector which maximum value is 1.0
+
+    while abs (ox - tx) > 1 or abs (oy - ty) > 1 or abs (oz - tz) > 1:
+        tx += dx
+        ty += dy
+        tz += dz
+        if (ty < 0 and dy < 0) or (ty >= 127 and dy > 0):
+            break
+        rx, ry, rz = int (tx), int (ty), int (tz)
+        yield rx, ry, rz
+
+def gen_line_covered (point1, point2):
+    """ This is Bresenham's algorithm with a little twist : *all* the blocks that
+        intersect with the line are yielded.
+    """
+    tx, ty, tz = point1.x, point1.y, point1.z # t is for temporary
+    rx, ry, rz = int (tx), int (ty), int (tz) # r is for rounded
+    ox, oy, oz = point2.x, point2.y, point2.z # o is for objective
+
+    dx = ox - tx
+    dy = oy - ty
+    dz = oz - tz
+
+    largest = max (abs (dx), abs (dy), abs (dz)) * 1. # We want a float
+    dx, dy, dz = dx / largest, dy / largest, dz / largest # We make a vector which maximum value is 1.0
+    adx, ady, adz = abs (dx), abs (dy), abs (dz)
+
+    px, py, pz = rx, ry, rz
+    while abs (ox - tx) > 1 or abs (oy - ty) > 1 or abs (oz - tz) > 1:
+        tx += dx
+        ty += dy
+        tz += dz
+        if (ty < 0 and dy < 0) or (ty >= 127 and dy > 0):
+            break
+        rx, ry, rz = int (tx), int (ty), int (tz)
+
+        yield rx, ry, rz
+
+        # Send blocks that are in fact intersected by the line
+        # but that bresenham skipped.
+        if rx != px and adx != 1:
+            yield px, ry, rz
+
+            if ry != py and ady != 1:
+                yield px, py, rz
+
+            if rz != pz and adz != 1:
+                yield px, ry, pz
+
+        if ry != py and ady != 1:
+            yield rx, py, rz
+
+            if rz != pz and adz != 1:
+                yield rx, py, pz
+
+        if rz != pz and adz != 1:
+            yield rx, ry, pz
+
+        px, py, pz = rx, ry, rz
