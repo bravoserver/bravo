de82ca12cb5fe74d58fe51307c10d290caf7f4cf
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-05-14 15:38:20 -0700

    tests: Fix policy test.

--- bravo/tests/policy/test_dig.py
+++ bravo/tests/policy/test_dig.py
@@ -1,6 +1,7 @@
 import unittest
 
-from bravo.blocks import blocks
+from bravo.blocks import blocks, items
+from bravo.inventory import Slot
 from bravo.policy.dig import dig_policies
 
 class TestNotchyDigPolicy(unittest.TestCase):
@@ -11,8 +12,18 @@ class TestNotchyDigPolicy(unittest.TestCase):
     def test_trivial(self):
         pass
 
+    def test_sapling_1ko(self):
+        self.assertTrue(self.p.is_1ko(blocks["sapling"].slot, None))
+
     def test_snow_1ko(self):
-        self.assertTrue(self.p.is_1ko(blocks["snow"].slot), None)
+        """
+        Snow can't be 1KO'd by hand, just with a shovel.
+        """
+
+        slot = Slot(items["wooden-shovel"].slot, 0x64, 1)
+
+        self.assertFalse(self.p.is_1ko(blocks["snow"].slot, None))
+        self.assertTrue(self.p.is_1ko(blocks["snow"].slot, slot))
 
     def test_dirt_bare(self):
         self.assertAlmostEqual(self.p.dig_time(blocks["dirt"].slot, None),
