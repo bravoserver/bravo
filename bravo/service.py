24129e955cbb574d9be32f039695caf6984bd3a0
Corbin Simpson <simpsoco@osuosl.org>
2011-06-21 15:33:13 -0700

    service: Don't clobber exported name.

--- bravo/service.py
+++ bravo/service.py
@@ -1,6 +1,6 @@
 from twisted.application.internet import TCPClient, TCPServer
 from twisted.application.service import Application, MultiService
-from twisted.application.strports import service
+from twisted.application.strports import service as serviceForEndpoint
 from twisted.internet.protocol import Factory
 from twisted.python import log
 
@@ -20,7 +20,7 @@ class BetaProxyFactory(Factory):
 def services_for_endpoints(endpoints, factory):
     l = []
     for endpoint in endpoints:
-        server = service(endpoint, factory)
+        server = serviceForEndpoint(endpoint, factory)
         # XXX hack for bravo.web:135, which wants this. :c
         server.args = [None, factory]
         server.setName("%s (%s)" % (factory.name, endpoint))
