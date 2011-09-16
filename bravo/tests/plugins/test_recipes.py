7e307e8af4272e951803e7e43757e9df568c4c3f
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-09-16 16:28:18 -0700

    tests: Add some recipe tests.
    Includes a test for #336.

--- bravo/tests/plugins/test_recipes.py
+++ bravo/tests/plugins/test_recipes.py
@@ -1,21 +1,79 @@
 # This test suite *does* require trial, for sane conditional test skipping.
 from twisted.trial import unittest
 
-from bravo.blocks import items
+from bravo.beta.structures import Slot
+from bravo.blocks import blocks, items
 from bravo.ibravo import IRecipe
 from bravo.plugin import retrieve_plugins
 
-class TestCompass(unittest.TestCase):
+class TestRecipes(unittest.TestCase):
 
     def setUp(self):
         self.p = retrieve_plugins(IRecipe)
-
-        if "compass" not in self.p:
-            raise unittest.SkipTest("Plugin not present")
 
     def test_trivial(self):
         pass
 
     def test_compass_provides(self):
+        if "compass" not in self.p:
+            raise unittest.SkipTest("Plugin not present")
+
         self.assertEqual(self.p["compass"].provides,
             (items["compass"].key, 1))
+
+    def test_black_wool_matches_white(self):
+        """
+        White wool plus an ink sac equals black wool.
+        """
+
+        if "black-wool" not in self.p:
+            raise unittest.SkipTest("Plugin not present")
+
+        table = [
+            Slot.from_key(blocks["white-wool"].key, 1),
+            Slot.from_key(items["ink-sac"].key, 1),
+            None,
+            None,
+        ]
+        self.assertTrue(self.p["black-wool"].matches(table, 2))
+
+    def test_black_wool_matches_lime(self):
+        """
+        Lime wool plus an ink sac equals black wool.
+        """
+
+        if "black-wool" not in self.p:
+            raise unittest.SkipTest("Plugin not present")
+
+        table = [
+            Slot.from_key(blocks["lime-wool"].key, 1),
+            Slot.from_key(items["ink-sac"].key, 1),
+            None,
+            None,
+        ]
+        self.assertTrue(self.p["black-wool"].matches(table, 2))
+
+    test_black_wool_matches_lime.todo = "Wool colors matter in recipes"
+
+    def test_bed_matches_tie_dye(self):
+        """
+        Three different colors of wool can be used to build beds.
+        """
+
+        if "bed" not in self.p:
+            raise unittest.SkipTest("Plugin not present")
+
+        table = [
+            None,
+            None,
+            None,
+            Slot.from_key(blocks["blue-wool"].key, 1),
+            Slot.from_key(blocks["red-wool"].key, 1),
+            Slot.from_key(blocks["lime-wool"].key, 1),
+            Slot.from_key(blocks["wood"].key, 1),
+            Slot.from_key(blocks["wood"].key, 1),
+            Slot.from_key(blocks["wood"].key, 1),
+        ]
+        self.assertTrue(self.p["bed"].matches(table, 3))
+
+    test_bed_matches_tie_dye.todo = "Test for #336"
