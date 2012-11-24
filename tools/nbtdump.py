fd9450fecf51fe0d15ac43827efb12c7e102e5a3
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-11-24 12:12:32 -0800

    tools/nbtdump: Fix up error message.

--- tools/nbtdump.py
+++ tools/nbtdump.py
@@ -5,7 +5,8 @@ import sys
 from bravo.nbt import NBTFile
 
 if len(sys.argv) < 2:
-    print "Usage: %s <file>" % __name__
+    print "Usage: %s <file>" % sys.argv[0]
+    sys.exit()
 
 f = NBTFile(sys.argv[1])
 print f.pretty_tree()
