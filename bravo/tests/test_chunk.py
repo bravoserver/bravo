f8cafb5466414509e0e8773a59b5593335d2f3e0
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-12-08 22:36:41 -0800

    tests/test_chunk: Remove numpy-specific tests.

--- bravo/tests/test_chunk.py
+++ bravo/tests/test_chunk.py
@@ -89,44 +89,6 @@ class TestChunkBlocks(unittest.TestCase):
         self.c.destroy((0, 30, 0))
         self.assertEqual(self.c.heightmap[0], 20)
 
-class TestNumpyQuirks(unittest.TestCase):
-    """
-    Tests for the bad interaction between several components of Bravo.
-
-    To be specific, these tests ensure that numpy objects are never leaked to
-    construct, which cannot deal with numpy's automatic sizing.
-    """
-
-    def test_get_damage_packet_single(self):
-        # Create a chunk.
-        c = Chunk(0, 0)
-        # Damage the block.
-        c.populated = True
-        c.set_block((0, 0, 0), 1)
-        # Enable warning-to-error for DeprecationWarning, then see whether
-        # retrieving damage causes a warning-to-error to be raised. (It
-        # shouldn't.)
-        warnings.simplefilter("error", DeprecationWarning)
-        c.get_damage_packet()
-        # ...And reset the warning filters.
-        warnings.resetwarnings()
-
-    def test_get_damage_packet_batch(self):
-        # Create a chunk.
-        c = Chunk(0, 0)
-        # Damage the block so that it will generate a multi-block batch
-        # packet.
-        c.populated = True
-        c.set_block((0, 0, 0), 1)
-        c.set_block((0, 0, 1), 1)
-        # Enable warning-to-error for DeprecationWarning, then see whether
-        # retrieving damage causes a warning-to-error to be raised. (It
-        # shouldn't.)
-        warnings.simplefilter("error", DeprecationWarning)
-        c.get_damage_packet()
-        # ...And reset the warning filters.
-        warnings.resetwarnings()
-
 class TestLightmaps(unittest.TestCase):
 
     def setUp(self):
