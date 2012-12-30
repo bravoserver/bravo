74e06e2c65baa992add70236727cb7ff43061d5f
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-12-30 12:34:55 -0800

    Add a tool for dumping the contents of regions.

---
+++ tools/regiondump.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from __future__ import division
+
+import sys
+
+from twisted.python.filepath import FilePath
+
+from bravo.region import Region
+
+if len(sys.argv) < 2:
+    print "No path specified!"
+    sys.exit()
+
+fp = FilePath(sys.argv[1])
+
+if not fp.exists():
+    print "Region %r doesn't exist!" % fp.path
+    sys.exit()
+
+region = Region(fp)
+region.load_pages()
+
+for (x, z) in region.positions:
+    length, version = region.get_chunk_header(x, z)
+    print "(%d, %d): v%d, %.2fKiB" % (x, z, version, length / 1024)
