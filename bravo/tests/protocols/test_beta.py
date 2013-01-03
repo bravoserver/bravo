3c85a4d1868ae16f3c204280e364935dd7132795
Corbin Simpson <MostAwesomeDude@gmail.com>
2013-01-03 12:27:31 -0800

    tests: Add tests for ascend().
    Turns out it's broken! Who knew?
    
    Refs #390.

--- bravo/tests/protocols/test_beta.py
+++ bravo/tests/protocols/test_beta.py
@@ -1,4 +1,4 @@
-from twisted.trial import unittest
+from twisted.trial.unittest import TestCase
 
 import warnings
 
@@ -8,9 +8,9 @@ from twisted.internet.task import deferLater
 from construct import Container
 
 from bravo.beta.protocol import BetaServerProtocol, BravoProtocol
+from bravo.chunk import Chunk
 from bravo.config import BravoConfigParser
 from bravo.errors import BetaClientError
-from bravo.location import Position
 
 class FakeTransport(object):
 
@@ -28,7 +28,7 @@ class FakeFactory(object):
     def broadcast(self, packet):
         pass
 
-class TestBetaServerProtocol(unittest.TestCase):
+class TestBetaServerProtocol(TestCase):
 
     def setUp(self):
         self.p = BetaServerProtocol()
@@ -128,7 +128,7 @@ class TestBetaServerProtocol(unittest.TestCase):
             self.p.latency = 70000
 
 
-class TestBravoProtocol(unittest.TestCase):
+class TestBravoProtocol(TestCase):
 
     def setUp(self):
         self.bcp = BravoConfigParser()
@@ -156,3 +156,54 @@ class TestBravoProtocol(unittest.TestCase):
         """
 
         self.p.disable_chunk(0, 0)
+
+
+class TestBravoProtocolChunks(TestCase):
+
+    def setUp(self):
+        self.bcp = BravoConfigParser()
+        self.p = BravoProtocol(self.bcp, "unittest")
+        self.p.setTimeout(None)
+
+    def test_trivial(self):
+        pass
+
+    def test_ascend_zero(self):
+        """
+        ``ascend()`` can take a count of zero to ensure that the client is
+        standing on solid ground.
+        """
+
+        self.p.location.pos = self.p.location.pos._replace(y=16)
+        c = Chunk(0, 0)
+        c.set_block((0, 0, 0), 1)
+        self.p.chunks[0, 0] = c
+        self.p.ascend(0)
+        self.assertEqual(self.p.location.pos.y, 16)
+
+    def test_ascend_zero_up(self):
+        """
+        Even with a zero count, ``ascend()`` will move the player to the
+        correct elevation.
+        """
+
+        self.p.location.pos = self.p.location.pos._replace(y=16)
+        c = Chunk(0, 0)
+        c.set_block((0, 0, 0), 1)
+        c.set_block((0, 1, 0), 1)
+        self.p.chunks[0, 0] = c
+        self.p.ascend(0)
+        self.assertEqual(self.p.location.pos.y, 32)
+
+    def test_ascend_one_up(self):
+        """
+        ``ascend()`` moves players upwards.
+        """
+
+        self.p.location.pos = self.p.location.pos._replace(y=16)
+        c = Chunk(0, 0)
+        c.set_block((0, 0, 0), 1)
+        c.set_block((0, 1, 0), 1)
+        self.p.chunks[0, 0] = c
+        self.p.ascend(1)
+        self.assertEqual(self.p.location.pos.y, 32)
